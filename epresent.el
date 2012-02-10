95a8eeccbd3803767a1cb3b5f21d163644ae7fda
Tero Hasu <tero.hasu@hut.fi>
2012-02-10 01:41:29 +0100

    customizations, with most rebased on top of latest upstream version
        decrease latex scaling factor
    
        do not unset file modified flag (might lose actual changes)
    
        replace use of text-scale-adjust with text-scale-increase
        text-scale-adjust is an interactive function.
    
        no mode line config option
    
        fix two latex fragment bugs
    
        fix inline image display state after EPresent mode
    
        The command C-c C-x C-v (org-toggle-inline-images) was ineffective
        after using EPresent. Now inline images are always turned on for
        presentations, and always turned off afterwards.
    
        make indented # non-comments visible
        support for indented #+ comments
    
        gitignore updates
    
        date instead of version
    
        adjust commentary; copyright addition
    
        update readme
    
        fixes to bottom, top, and goto top level, and a debug info command
    
        b command for jumping to bottom of document
    
        not showing in a separate frame
    
        not fullscreen, smaller title font
    
        better current page function, bound
    
        visible cursor, no left/right keybinding override
    
        current page keybinding

--- epresent.el
+++ epresent.el
@@ -2,10 +2,13 @@
 
 ;; Copyright (C) 2008 Tom Tromey <tromey@redhat.com>
 ;;               2010 Eric Schulte <schulte.eric@gmail.com>
+;;               2011 Tero Hasu <tero.hasu@hut.fi>
 
-;; Authors: Tom Tromey <tromey@redhat.com>, Eric Schulte <schulte.eric@gmail.com>
+;; Authors: Tom Tromey <tromey@redhat.com>, 
+;;          Eric Schulte <schulte.eric@gmail.com>,
+;;          Tero Hasu <tero.hasu@hut.fi>
 ;; Created: 12 Jun 2008
-;; Version: 0.1
+;; Last modified: much later
 ;; Keywords: gui
 
 ;; This file is not (yet) part of GNU Emacs.
@@ -31,11 +34,10 @@
 ;; This is a simple presentation mode for Emacs.  It works best in
 ;; Emacs 23, which has nicer font rendering.
 
-;; To use, invoke epresent-run in an org-mode buffer.  This
-;; will make a full-screen frame.  Use n/p to navigate, or q to quit.
-;; (There are some other key bindings too.)  Each top-level headline
-;; becomes a page in the presentation and Org-mode markup is used
-;; to nicely display the buffer's contents.
+;; To use, invoke epresent-run in an org-mode buffer. Use n/p to
+;; navigate, or q to quit. (There are some other key bindings too.)
+;; Each top-level headline becomes a page in the presentation and
+;; Org-mode markup is used to nicely display the buffer's contents.
 
 ;;; Code:
 (require 'org)
@@ -43,7 +45,7 @@
 (require 'org-latex)
 
 (defface epresent-title-face
-  '((t :weight bold :height 2.4 :underline t :inherit variable-pitch))
+  '((t :weight bold :height 2.2 :underline t :inherit variable-pitch))
   "")
 (defface epresent-heading-face
   '((t :weight bold :height 1.8 :underline t :inherit variable-pitch))
@@ -71,7 +73,6 @@
 
 (defvar epresent-overlays nil)
 
-(defvar epresent-inline-image-overlays nil)
 (defvar epresent-src-fontify-natively nil)
 (defvar epresent-hide-emphasis-markers nil)
 (defvar epresent-format-latex-scale nil)
@@ -82,61 +83,46 @@
 (defvar epresent-frame-level 1)
 (make-variable-frame-local 'epresent-frame-local) ;; Obsolete function?
 
-(defvar epresent-mode-line nil
-  "Set the mode-line format. Hides it when nil")
-
 (defun epresent--get-frame ()
   (unless (frame-live-p epresent--frame)
     (setq epresent--frame (make-frame '((minibuffer . nil)
                                         (title . "EPresent")
-                                        (fullscreen . fullboth)
+                                        ;;(fullscreen . fullboth)
                                         (menu-bar-lines . 0)
                                         (tool-bar-lines . 0)
                                         (vertical-scroll-bars . nil)
                                         (left-fringe . 0)
                                         (right-fringe . 0)
                                         (internal-border-width . 20)
-                                        (cursor-type . nil)
+                                        ;;(cursor-type . nil)
                                         ))))
   (raise-frame epresent--frame)
   (select-frame-set-input-focus epresent--frame)
   epresent--frame)
 
-;; functions
-(defun epresent-get-frame-level ()
-  "Get the heading level to show as different frames."
-  (interactive)
-  (save-excursion
-    (save-restriction
-      (widen)
-      (goto-char (point-min))
-      (if (re-search-forward
-           "^#\\+EPRESENT_FRAME_LEVEL:[ \t]*\\(.*?\\)[ \t]*$" nil t)
-          (string-to-number (match-string 1))
-        1))))
-
 (defun epresent-goto-top-level ()
   "Go to the current top level heading containing point."
   (interactive)
-  (unless (org-at-heading-p) (outline-previous-heading))
-  (let ((level (ignore-errors (org-reduced-level (org-current-level)))))
-    (when (and level (> level epresent-frame-level))
-      (org-up-heading-all (- level epresent-frame-level)))))
+  (unless (org-at-heading-p) 
+    (outline-previous-heading))
+  (let ((level (org-current-level)))
+    (when (and level (> level 1))
+      ;; Some things are invisible when EPresenting, hence pass "t".
+      (outline-up-heading (- level 1) t))))
 
 (defun epresent-current-page ()
   "Present the current outline heading."
+  (when (org-current-level)
+    (epresent-goto-top-level)
+    (org-show-subtree)
+    (org-narrow-to-subtree)))
+
+(defun epresent-debug ()
+  "Show debug information."
   (interactive)
-  (if (org-current-level)
-      (progn
-        (epresent-goto-top-level)
-        (org-narrow-to-subtree)
-        (show-all)
-        (hide-body)
-        (when (>= (org-reduced-level (org-current-level))
-                  epresent-frame-level)
-          (org-show-subtree)))
-    ;; before first headline -- fold up subtrees as TOC
-    (org-cycle '(4))))
+  (message (format "lv:%s @h:%s" 
+		   (org-current-level)
+		   (org-at-heading-p))))
 
 (defun epresent-top ()
   "Present the first outline heading."
@@ -145,27 +131,38 @@
   (goto-char (point-min))
   (epresent-current-page))
 
+(defun epresent-bottom ()
+  "Present the last outline heading."
+  (interactive)
+  (beginning-of-line)
+  (widen)
+  (goto-char (point-max))
+  (epresent-current-page))
+
+(defun epresent-this-page ()
+  "Present the current outline heading."
+  (interactive)
+  (beginning-of-line)
+  (widen)
+  (epresent-goto-top-level)
+  (epresent-current-page))
+
 (defun epresent-next-page ()
   "Present the next outline heading."
   (interactive)
-  (epresent-goto-top-level)
+  (beginning-of-line)
   (widen)
-  (if (< (or (ignore-errors (org-reduced-level (org-current-level))) 0)
-         epresent-frame-level)
-      (outline-next-heading)
-    (org-get-next-sibling))
+  (epresent-goto-top-level)
+  (org-get-next-sibling)
   (epresent-current-page))
 
 (defun epresent-previous-page ()
   "Present the previous outline heading."
   (interactive)
-  (epresent-goto-top-level)
+  (beginning-of-line)
   (widen)
-  (org-content)
-  (if (< (or (ignore-errors (org-reduced-level (org-current-level))) 0)
-         epresent-frame-level)
-      (outline-previous-heading)
-    (org-get-last-sibling))
+  (epresent-goto-top-level)
+  (or (org-get-last-sibling) (goto-char (point-min)))
   (epresent-current-page))
 
 (defun epresent-clean-overlays ()
@@ -177,15 +174,15 @@
   "Quit the current presentation."
   (interactive)
   ;; restore the font size
-  (text-scale-adjust (/ 1 epresent-text-scale))
+  (text-scale-increase 0) ;; remove any active scaling
   (org-remove-latex-fragment-image-overlays)
+  (org-remove-inline-images)
   ;; restore the user's Org-mode variables
-  (setq org-inline-image-overlays epresent-inline-image-overlays)
   (setq org-src-fontify-natively epresent-src-fontify-natively)
   (setq org-hide-emphasis-markers epresent-hide-emphasis-markers)
   (plist-put org-format-latex-options :scale epresent-format-latex-scale)
-  (when (string= "EPresent" (frame-parameter nil 'title))
-    (delete-frame (selected-frame)))
+  ;;(when (string= "EPresent" (frame-parameter nil 'title))
+  ;;(delete-frame (selected-frame)))
   (when epresent--org-buffer
     (set-buffer epresent--org-buffer))
   (org-mode)
@@ -207,20 +204,30 @@
            '(epresent-heading-face epresent-content-face epresent-fixed-face))
     (set-face-attribute face nil :height (1- (face-attribute face :height)))))
 
+(defun epresent-push-new-overlay (p0 p1 nm val)
+  (let ((ol (make-overlay p0 p1)))
+    (overlay-put ol nm val)
+    (push ol epresent-overlays)))
+
 (defun epresent-fontify ()
   "Overlay additional presentation faces to Org-mode."
   (save-excursion
     ;; hide all comments
     (goto-char (point-min))
+    ;; xxx could we just temporarily modify the Org-mode face for comments
     (while (re-search-forward
-            "^[ \t]*#\\(\\+\\(author\\|title\\|date\\):\\)?.*\n"
+            "^\\(#\\|[ \t]*#\\+\\).*\n"
             nil t)
-      (unless (and (match-string 2)
-                   (save-match-data
-                     (string-match (regexp-opt '("title" "author" "date"))
-                                   (match-string 2))))
-        (push (make-overlay (match-beginning 0) (match-end 0)) epresent-overlays)
-        (overlay-put (car epresent-overlays) 'invisible 'epresent-hide)))
+      (let ((comment (match-string 0))
+	    (cb (match-beginning 0))
+	    (ce (match-end 0)))
+	(unless (save-match-data 
+		  ;; (regexp-opt '("title" "author" "date"))
+		  (string-match 
+		   "^#\\+\\(author\\|title\\|date\\):"
+		   comment))
+	  (epresent-push-new-overlay cb ce 'invisible 'epresent-hide))))
+
     ;; page title faces
     (goto-char (point-min))
     (while (re-search-forward "^\\(*+\\)[ \t]*\\(.*\\)$" nil t)
@@ -271,38 +278,40 @@
         (overlay-put
          (car epresent-overlays) 'face (intern (format "epresent-%s-face" el)))))
     ;; inline images
-    (org-display-inline-images)))
+    (unless org-inline-image-overlays
+      (org-display-inline-images))))
 
 (defvar epresent-mode-map
   (let ((map (make-keymap)))
     (suppress-keymap map)
     (define-key map " " 'epresent-next-page)
+    (define-key map "b" 'epresent-bottom)
+    (define-key map "c" 'epresent-this-page)
+    (define-key map "d" 'epresent-debug)
     (define-key map "n" 'epresent-next-page)
-    (define-key map [right] 'epresent-next-page)
+    ;;(define-key map [right] 'epresent-next-page)
     (define-key map "p" 'epresent-previous-page)
-    (define-key map [left] 'epresent-previous-page)
+    ;;(define-key map [left] 'epresent-previous-page)
     (define-key map [backspace] 'epresent-previous-page)
     (define-key map "q" 'epresent-quit)
-    (define-key map "1" 'epresent-top)
+    ;;(define-key map "1" 'epresent-top)
     (define-key map "t" 'epresent-top)
     map)
   "Local keymap for EPresent display mode.")
 
 (define-derived-mode epresent-mode org-mode "EPresent"
   "Lalala."
-  (text-scale-adjust 0)
-  (text-scale-adjust epresent-text-scale)
+  (text-scale-increase 0) ;; remove any active scaling
+  (text-scale-increase epresent-text-scale)
   ;; make Org-mode be as pretty as possible
-  (setq epresent-inline-image-overlays org-inline-image-overlays)
   (setq epresent-src-fontify-natively org-src-fontify-natively)
   (setq org-src-fontify-natively t)
   (setq epresent-hide-emphasis-markers org-hide-emphasis-markers)
   (setq org-hide-emphasis-markers t)
-  (setq mode-line-format epresent-mode-line)
   (setq epresent-format-latex-scale (plist-get org-format-latex-options :scale))
-  (let ((org-format-latex-options
-         (plist-put org-format-latex-options :scale 4.0)))
-    (org-preview-latex-fragment 16))
+  (setq org-format-latex-options
+	(plist-put org-format-latex-options :scale 3.0))
+  (org-preview-latex-fragment '(16))
   ;; fontify the buffer
   (add-to-invisibility-spec '(epresent-hide))
   ;; remove flyspell overlays
@@ -315,13 +324,14 @@
   (interactive)
   (unless (eq major-mode 'org-mode)
     (error "EPresent can only be used from Org Mode"))
-  (setq epresent-frame-level (epresent-get-frame-level))
+  ;; For this to work we must do it early, but do not know why. If we
+  ;; don't, and if C-c C-x C-l is active, we retain small size preview
+  ;; images, and latex preview state is messed up upon presentation
+  ;; exit. Possibly switching modes will change so much state that
+  ;; this is no longer effective at that point.
+  (org-remove-latex-fragment-image-overlays)
   (setq epresent--org-buffer (current-buffer))
-  (epresent--get-frame)
-  (epresent-mode)
-  (set-buffer-modified-p nil))
-
-;;;###autoload(global-set-key [f12] 'epresent-run)
+  (epresent-mode))
 
 (provide 'epresent)
 ;;; epresent.el ends here
